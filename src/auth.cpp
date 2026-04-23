14b67fb527a22d6eef97f0343139b046fd83a5cb
No1d3d <nikita.pereverzin@gmail.com>
2026-04-23 11:18:59 +0300

    windows done, tests pass

--- src/auth.cpp
+++ src/auth.cpp
@@ -2,12 +2,10 @@
 
 #include <openssl/evp.h>
 #include <openssl/pem.h>
-#include <openssl/rsa.h>
 
 #include <vector>
-#include <iostream>
 
-// Зашитый приватный ключ
+// Embedded private key
 static const char* PRIVATE_KEY_PEM = R"(-----BEGIN PRIVATE KEY-----
 MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAxpvV3iqUTK3sZNK2
 MnCDeNF/3u3XUEBaNicPaK5nQnX0FjWxVHgio/ppb+TjAUB4DOvmSW6yVSw5quw1
@@ -54,24 +52,59 @@ bool verify_public_key(const std::string& public_key_pem)
     const std::string msg = "auth_check";
 
     EVP_MD_CTX* sign_ctx = EVP_MD_CTX_new();
-    if (!sign_ctx) return false;
+    if (!sign_ctx)
+    {
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
+        return false;
+    }
 
     if (EVP_DigestSignInit(sign_ctx, nullptr, EVP_sha256(), nullptr, priv) != 1)
+    {
+        EVP_MD_CTX_free(sign_ctx);
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
         return false;
+    }
 
     EVP_DigestSignUpdate(sign_ctx, msg.data(), msg.size());
 
     size_t sig_len = 0;
-    EVP_DigestSignFinal(sign_ctx, nullptr, &sig_len);
+    if (EVP_DigestSignFinal(sign_ctx, nullptr, &sig_len) != 1)
+    {
+        EVP_MD_CTX_free(sign_ctx);
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
+        return false;
+    }
 
     std::vector<unsigned char> sig(sig_len);
-    EVP_DigestSignFinal(sign_ctx, sig.data(), &sig_len);
+    if (EVP_DigestSignFinal(sign_ctx, sig.data(), &sig_len) != 1)
+    {
+        EVP_MD_CTX_free(sign_ctx);
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
+        return false;
+    }
+
     EVP_MD_CTX_free(sign_ctx);
 
     EVP_MD_CTX* verify_ctx = EVP_MD_CTX_new();
-    if (!verify_ctx) return false;
+    if (!verify_ctx)
+    {
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
+        return false;
+    }
 
-    EVP_DigestVerifyInit(verify_ctx, nullptr, EVP_sha256(), nullptr, pub);
+    if (EVP_DigestVerifyInit(verify_ctx, nullptr, EVP_sha256(), nullptr, pub) != 1)
+    {
+        EVP_MD_CTX_free(verify_ctx);
+        EVP_PKEY_free(priv);
+        EVP_PKEY_free(pub);
+        return false;
+    }
+
     EVP_DigestVerifyUpdate(verify_ctx, msg.data(), msg.size());
 
     int ok = EVP_DigestVerifyFinal(verify_ctx, sig.data(), sig_len);
--- tests/test_auth.cpp
+++ tests/test_auth.cpp
@@ -1,69 +1,28 @@
 #include <gtest/gtest.h>
-#include <cstdio>
-#include <memory>
-#include <array>
+#include "auth.h"
 
-#ifdef _WIN32
-    #define popen _popen
-    #define pclose _pclose
-#endif
-
-#ifdef _WIN32
-    std::string exe_path = "auth_app.exe";
-#else
-    std::string exe_path = "./auth_app";
-#endif
-
-std::string cmd = exe_path + " some_args";
-
-std::string run(const std::string& cmd)
-{
-    std::array<char, 128> buf;
-    std::string result;
-
-    FILE* pipe = popen(cmd.c_str(), "r");
-    if (!pipe) return "";
-
-    while (fgets(buf.data(), buf.size(), pipe))
-        result += buf.data();
-
-    pclose(pipe);
-    return result;
-}
-
-// Валидный публичный ключ
+// Valid public key
 static const char* VALID_KEY = R"(-----BEGIN PUBLIC KEY-----
 MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMab1d4qlEyt7GTStjJwg3jRf97t11BA
 WjYnD2iuZ0J19BY1sVR4IqP6aW/k4wFAeAzr5kluslUsOarsNaxSTVsCAwEAAQ==
 -----END PUBLIC KEY-----)";
 
-// Невалидный ключ
+// Invalid key
 static const char* INVALID_KEY = R"(-----BEGIN PUBLIC KEY-----
-COOLINVALIDKEYFORTESTScoolinvalidkeyfortests123123
+INVALIDKEYDATA
 -----END PUBLIC KEY-----)";
 
-TEST(CLI, NoArgs)
+TEST(Auth, ValidKey)
 {
-    auto out = run(exe_path);
-    EXPECT_NE(out.find("FAILED"), std::string::npos);
+    EXPECT_TRUE(verify_public_key(VALID_KEY));
 }
 
-TEST(CLI, ValidKey)
+TEST(Auth, InvalidKey)
 {
-    std::string cmd = exe_path + " \"" + std::string(VALID_KEY) + "\"";
-    auto out = run(cmd);
-    EXPECT_NE(out.find("SUCCESS"), std::string::npos);
+    EXPECT_FALSE(verify_public_key(INVALID_KEY));
 }
 
-TEST(CLI, InvalidKey)
+TEST(Auth, EmptyKey)
 {
-    std::string cmd = exe_path + " \"" + std::string(INVALID_KEY) + "\"";
-    auto out = run(cmd);
-    EXPECT_NE(out.find("FAILED"), std::string::npos);
-}
-
-TEST(CLI, TooManyArgs)
-{
-    auto out = run(exe_path + " a b");
-    EXPECT_NE(out.find("FAILED"), std::string::npos);
+    EXPECT_FALSE(verify_public_key(""));
 }
